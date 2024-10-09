b3a0423d0155e33914537448838f82f41eac77d2
Eslam Alhendawy <eslam_alhendawy@outlook.com>
2024-10-09 17:16:22 +0300

    removed one of the lines in Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="bg-[#F4F3EE33] w-[1px] h-full absolute top-0 left-[56%] lg:left-[38%]" />
         <div className="bg-[#F4F3EE33] w-[1px] h-full absolute top-0 left-[70%] lg:left-[47.5%]" />
         <div className="bg-[#F4F3EE33] w-[1px] h-full absolute top-0 left-[84%] lg:left-[57%]" />
-        <div className="bg-[#F4F3EE33] w-[1px] h-full absolute top-0 left-[98%] lg:left-[66.5%] hidden lg:block" />
+        {/* <div className="bg-[#F4F3EE33] w-[1px] h-full absolute top-0 left-[98%] lg:left-[66.5%] hidden lg:block" /> */}
         <h1 className="font-goldman text-2xl sm:text-4xl lg:text-6xl xl:text-7xl 2xl:text-[80px] 2xl:w-[70%] font-bold capitalize mb-4 xl:mb-6 2xl:mb-8">intuitive, Streamlined, Comprehensive</h1>
         <p className="font-gothic font-semibold lg:border-l-[3px] border-white lg:pl-2 sm:text-lg lg:text-xl xl:text-2xl xl:w-[80%] 2xl:w-[70%]">This real estate system offers a streamlined and intuitive platform for buyers & sellers. It features advanced search options, interactive tools, and personalized recommendations. Users can easily explore properties, and connect with professionals.</p>
       </div>
