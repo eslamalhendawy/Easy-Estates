bef80b3b4498e0a624c27359282ac9208bff7a0d
Eslam Alhendawy <eslam_alhendawy@outlook.com>
2024-10-18 14:32:05 +0300

    Finishing touches to Properties and AboutUs pages

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -10,9 +10,9 @@ const Properties = () => {
         <div className="flex items-center justify-between gap-2 border border-greyColor rounded-xl lg:w-[40%] 2xl:w-[30%]">
           <div className="flex items-center gap-2 ml-2">
             <img src={locationDot} alt="" />
-            <input className="outline-none" type="text" />
+            <input className="outline-none" type="text" placeholder="Filter By Location" />
           </div>
-          <button className="bg-black hover:bg-[#403d39] duration-200 py-2 px-2 rounded-xl flex items-center gap-2">
+          <button className="bg-black hover:bg-[#403d39] duration-200 py-2 px-2 rounded-xl flex items-center justify-center gap-2 xl:w-[20%]">
             <img className="size-[20px]" src={filter} alt="" />
             <span className="text-white font-semibold ">Filter</span>
           </button>
